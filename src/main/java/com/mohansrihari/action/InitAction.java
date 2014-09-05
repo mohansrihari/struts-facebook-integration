0e6bb5e326ac0917ddd22e4ad368c7f3ecd032cf
mohansrihari <k.mohansrihari@gmail.com>
2014-09-05 16:32:01 +0530

    sample

--- src/main/java/com/mohansrihari/action/InitAction.java
+++ src/main/java/com/mohansrihari/action/InitAction.java
@@ -4,7 +4,7 @@
 package com.mohansrihari.action;
 
 
-import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequest; 
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
